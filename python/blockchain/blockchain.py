6bbf855c324d2df93793982ee67a8be1a57bb039
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-03-28 02:08:37 +0900

    add blockchain utils

--- python/blockchain/blockchain.py
+++ python/blockchain/blockchain.py
@@ -1,6 +1,10 @@
 import logging
+import hashlib
+import json
 import sys
 import time
+
+import utils
 
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
@@ -11,16 +15,30 @@ class BlockChain(object):
     def __init__(self) -> None:
         self.transaction_pool = []
         self.chain = []
-        self.create_block(0, 'init hash')
+        self.create_block(0, self.hash({}))
 
     def create_block(self, nonce, previous_hash):
-        block = {
+        block = utils.sorted_dict_by_key({
             'timestamp': time.time(),
             'transactions': self.transaction_pool,
             'nonce': nonce,
             'previous_hash': previous_hash,
-        }
+        })
         self.chain.append(block)
         self.transaction_pool = []
         return block
+
+    def hash(self, block):
+        sorted_block = json.dumps(block, sort_keys=True)
+        return hashlib.sha256(sorted_block.encode()).hexdigest()
+
+    def add_transaction(self, sender_blockchain_address,
+                        recipient_blockchain_address, value):
+        transaction = utils.sorted_dict_by_key({
+            'sender_blockchain_address': sender_blockchain_address,
+            'recipient_blockchain_address': recipient_blockchain_address,
+            'value': float(value)
+        })
+        self.transaction_pool.append(transaction)
+        return True
         
