2822a1f6fa44b71ffbae230650613a224fa7d704
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-04-21 05:50:19 +0900

    add react

--- react-basic/src/components/ApiFetch.js
+++ react-basic/src/components/ApiFetch.js
@@ -3,25 +3,35 @@ import axios from 'axios'
 
 const ApiFetch = () => {
   const [posts, setPosts] = useState([])
+  const [id, setId] = useState(1)
+  const [clicked, setClicked] = useState(false)
+  const handlerCliked = () => {
+    setClicked(!clicked)
+  }
   useEffect(() => {
     // axios.get('https://jsonplaceholder.typicode.com/posts')
     // .then(res => {
     //     setPosts(res.data)
     // })
-    fetch('https://jsonplaceholder.typicode.com/posts', {method: 'GET'})
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {method: 'GET'})
     .then(res => res.json())
     .then(data => {
         setPosts(data)
     })
-  }, [])
+  }, [clicked])
   return (
     <div>
-        <ul>
+        <input type='text' value={id} onChange={evt=>setId(evt.target.value)}/>
+        <br/>
+        <button type='button' onClick={handlerCliked}>Get post</button>
+        <br/>
+        {posts.title}
+        {/* <ul>
             {
                 posts.map(post => <li key={post.id}>{post.title}</li>)
             }
-        </ul>
-    </div>
+        </ul>*/}
+    </div> 
   )
 }
 
