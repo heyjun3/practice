49d3f77b0dedd22f26d5c9a7c50588c836384187
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-04-25 06:06:59 +0900

    add react

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -4,29 +4,36 @@ import './App.css';
 // import BasicUseEffect from './components/BasicUseEffect';
 // import TimerContainer from './components/TimerContainer';
 import ApiFetch from './components/ApiFetch';
+import AppContext from './contexts/AppContext';
+import B from './components/B';
+import BasicReducer from './components/BasicReducer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <Basic2 name="Hooks"/> */}
-        {/* <BasicUseEffect /> */}
-        {/* <TimerContainer /> */}
-        <ApiFetch />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
-    </div>
+    <AppContext.Provider value={'value from App.js'}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <Basic2 name="Hooks"/> */}
+          {/* <BasicUseEffect /> */}
+          {/* <TimerContainer /> */}
+          {/* <ApiFetch />
+          <B /> */}
+          {/* <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+          <BasicReducer />
+        </header>
+      </div>
+    </AppContext.Provider>
   );
 }
 
