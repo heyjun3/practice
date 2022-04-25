511bd3fc012f0026df801d8bc84c0f77bfac5b8a
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-04-26 08:25:02 +0900

    fix comments

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -14,22 +14,6 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {/* <Basic2 name="Hooks"/> */}
-          {/* <BasicUseEffect /> */}
-          {/* <TimerContainer /> */}
-          {/* <ApiFetch />
-          <B /> */}
-          {/* <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
           <BasicReducer />
         </header>
       </div>
