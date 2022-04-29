6569fe3bf99272d37829b164807b6b611387a1f9
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-04-30 06:17:04 +0900

    add combinereducer

---
+++ redux/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useReducer} from 'react'
+
+import rootReducer from './reducers';
+import {SELL_MEAT, SELL_VEGETABLE} from './reducers/actionTypes'
+
+function App() {
+  const initalState = {
+    reducerMeat: {numOfMeat: 30},
+    reducerVegetable: {numOfVegetable: 25},
+  }
+  const [state, dispatch] = useReducer(rootReducer, initalState)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={()=>dispatch({type: SELL_MEAT})}>Sell meat</button>
+        Today's Meat: {state.reducerMeat.numOfMeat}
+        <button onClick={()=>dispatch({type: SELL_VEGETABLE})}>Sell vegetable</button>
+        Today's Vegetable: {state.reducerVegetable.numOfVegetable}
+      </header>
+    </div>
+  );
+}
+
+export default App;
