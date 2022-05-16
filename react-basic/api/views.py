a58aac555c5939c2c6d10613fa7c70331cbabf86
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-05-17 06:49:25 +0900

    add api.views

--- react-basic/api/views.py
+++ react-basic/api/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from tokenize import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
+from rest_framework import viewsets
+from django.contrib.auth.models import User
 
-# Create your views here.
+from api.models import Task 
+from api.serializers import UserSerializer
+from api.serializers import TaskSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny, )
+
+
+class ManageUserView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+
+    def get_object(self):
+        return self.request.user
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
