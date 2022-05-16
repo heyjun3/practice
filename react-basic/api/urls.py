ef058ea91ec3f197807b391f3163ceaa127da0eb
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-05-17 06:49:38 +0900

    create api urls

---
+++ react-basic/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+
+from api.views import UserViewSet
+from api.views import TaskViewSet
+from api.views import ManageUserView
+
+
+router = routers.DefaultRouter()
+router.register('tasks', TaskViewSet)
+router.register('users', UserViewSet)
+
+urlpattern = [
+    path('myself/', ManageUserView.as_view(), name='myself'),
+    path('', include(router.urls)),
+]
