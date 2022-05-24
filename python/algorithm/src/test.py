a1e16b7d2c8b08b6f7f598288327fa1300c7ae1a
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-05-24 09:40:48 +0900

    test decorator

--- python/algorithm/src/test.py
+++ python/algorithm/src/test.py
@@ -1,11 +1,21 @@
+from ast import Call
+from typing import Callable
+from typing import Any
 
-def func2(n: float):
-    if n <= 1:
-        return
-    else:
-        print(n)
-        func2(n/2)
+
+
+def hello(func: Callable) -> Callable:
+    print('hello func')
+    def _func1(*args):
+        print('hello')
+        return func(*args)
+    return _func1
+
+
+@hello
+def func1(num: Any) -> int:
+    return num ** num
 
 
 if __name__ == '__main__':
-    func2(10)
+    print(func1)
