77f391eedaff9e6f3bb1af9f24cfa73f743db32e
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-05-30 22:03:14 +0900

    fix asyncio

--- python/asincio/lesson_asyncio.py
+++ python/asincio/lesson_asyncio.py
@@ -27,23 +27,30 @@ async def hello_2(url):
             print(response)
             print(time.time())
 
-async def worker1(lock):
+async def worker1(event):
     print('worker1 start')
-    async with lock:
-        print('worker1 got lock')
-        await asyncio.sleep(3)
+    await event.wait()
+    print('worker1 got event')
+    await asyncio.sleep(3)
     print('worker1 end')
 
-async def worker2(lock):
+async def worker2(event):
     print('worker2 start')
-    async with lock:
-        print('worker2 got lock')
-        await asyncio.sleep(3)
+    await event.wait()
+    print('worker2 got event')
+    await asyncio.sleep(3)
     print('worker2 end')
+ 
+async def worker3(event):
+    print('worker3 start')
+    print('worker3 got event')
+    await asyncio.sleep(3)
+    print('worker3 end')
+    event.set()
 
 if __name__ == '__main__':
-    lock = asyncio.Lock()
+    event = asyncio.Event()
     loop.run_until_complete(asyncio.wait([
-        worker1(lock), worker2(lock),
+        worker1(event), worker2(event), worker3(event),
         ]))
     loop.close()
