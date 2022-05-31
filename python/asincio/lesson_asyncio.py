bb741b497bb0b376adcdb7416a0b36d1d41896e5
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-05-31 21:13:31 +0900

    fix asyncio.py

--- python/asincio/lesson_asyncio.py
+++ python/asincio/lesson_asyncio.py
@@ -48,9 +48,29 @@ async def worker3(event):
     print('worker3 end')
     event.set()
 
+
+class AwaitableClass(object):
+    def __init__(self, name, loop) -> None:
+        self.name = name
+        self.loop = loop
+
+    def __await__(self):
+        reader, writer = yield from asyncio.open_connection(
+            '127.0.0.1', 8888, loop=self.loop,
+        )
+        writer.write(self.name.encode())
+        writer.write_eof()
+        data = yield from reader.read()
+        return data
+
+
+async def main(name, loop):
+    print('chunk reader')
+    result = await AwaitableClass(name, loop)
+    print(result)
+
 if __name__ == '__main__':
-    event = asyncio.Event()
     loop.run_until_complete(asyncio.wait([
-        worker1(event), worker2(event), worker3(event),
+        main('task1', loop), main('task2', loop),
         ]))
     loop.close()
