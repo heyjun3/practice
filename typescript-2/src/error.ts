812899a08c515c2737166c97b56d412e4e70f7ac
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-06-09 22:49:36 +0900

    add TS

--- typescript-2/src/error.ts
+++ typescript-2/src/error.ts
@@ -15,3 +15,21 @@ const fail_1 = {
     bar: getBar(),
 }
 takeSomethingComplex(fail_1)
+
+type Id<T extends string> = {
+    type: T,
+    value: string,
+}
+
+type FooId = Id<'foo'>
+type BarId = Id<'bar'>
+
+const createFoo = (value: string): FooId => ({type: 'foo', value});
+const createBar = (value: string): BarId => ({type: 'bar', value});
+
+let foo = createFoo('sample')
+let bar = createBar('sample')
+
+// foo = bar;
+foo = foo;
+console.log(foo)
