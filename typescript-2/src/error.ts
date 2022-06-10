c3e25d5cdb04f06ca976a08ef04c941866af981b
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-06-10 23:00:32 +0900

    add TS

--- typescript-2/src/error.ts
+++ typescript-2/src/error.ts
@@ -33,3 +33,58 @@ let bar = createBar('sample')
 // foo = bar;
 foo = foo;
 console.log(foo)
+
+const callable = new class {
+    count = 0;
+    called = () => {
+        this.count++;
+        console.log(`Called : ${this.count}`)
+    }
+};
+
+callable.called()
+callable.called()
+
+const func: Function = (...args: number[]) => {
+    console.log(args)
+}
+
+func('1')
+
+let add = (x: number) => (y: number) => x + y;
+
+let add123 = add(123)
+console.log(add123(1))
+
+let foos = {
+    bar: 456
+}
+foos['bar'] = 123
+foos.bar = 1234
+console.log(foos)
+
+let foody = {} as any;
+foody.bar = 12344
+console.log(foody)
+
+interface Food {
+    bar: number,
+}
+
+let food = {} as Food
+food.bar = 123333
+console.log(food)
+
+function foosfoo() {
+    let property: number = 0;
+
+    function addProperty(): number{
+        let num = property + 1;
+        return num
+    }
+    return addProperty
+}
+
+const foosfo = foosfoo()
+let varialbe = foosfo()
+console.log(varialbe)
