1d452fb5887835614ecda2e7c037e3dd227ae151
heyjun3 <t3k2g5kako@yahoo.co.jp>
2022-06-16 09:55:45 +0900

    add asyncio310.py

---
+++ python/asincio/asyncio310.py
@@ -0,0 +1,71 @@
+import asyncio
+import time
+import datetime
+
+async def main():
+    print(f'started at {time.strftime("%X")}')
+    
+    await say_after(1, 'hello')
+    await say_after(2, 'world')
+
+    print(f'finished at {time.strftime("%X")}')
+
+
+async def say_after(delay: int, what: str) -> None:
+    await asyncio.sleep(delay)
+    print(what)
+
+async def main_1():
+    task1 = asyncio.create_task(say_after(1, 'hello'))
+    task2 = asyncio.create_task(say_after(2, 'world'))
+
+    print(f'started at {time.strftime("%X")}')
+    await task1
+    await task2
+    print(f'finished at {time.strftime("%X")}')
+
+async def nested():
+    return 42
+
+async def main_2():
+    task = asyncio.create_task(nested())
+    await task
+
+
+async def display_date():
+    loop = asyncio.get_running_loop()
+    end_time = loop.time() + 5.0
+    while True:
+        print(datetime.datetime.now())
+        if (loop.time() + 1.0) > end_time:
+            break
+        await asyncio.sleep(1)
+
+async def factorial(name, number):
+    f = 1
+    for i in range(2, number + 1):
+        print(f'Task {name}: Compute factorial({number}), currently i={i}...')
+        await asyncio.sleep(1)
+        f *= i
+    print(f'Task {name}: factorial({number}) = {f}')
+    return f
+
+async def main_3():
+    L = await asyncio.gather(
+        factorial('A', 2),
+        factorial('B', 3),
+        factorial('C', 4),
+    )
+    print(L)
+
+async def eternity():
+    await asyncio.sleep(3500)
+    print('yay!')
+
+async def main_4():
+    try:
+        await asyncio.wait_for(eternity(), timeout=1.0)
+    except asyncio.TimeoutError:
+        print('timeout!!')
+
+asyncio.run(main_4())
